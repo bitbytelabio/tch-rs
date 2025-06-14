5cb5aac980a10719990c2170ed837cff6239df9d
Jason Nguyen <dat.nguyen@bitbytelab.io>
2025-06-14 23:18:37 +0700

    Refactor torch_python.cpp and torch_python.h for consistency and readability
    - Updated function signatures in torch_python.cpp to use consistent spacing.
    - Removed unnecessary PROTECT blocks in thp_variable_check, thp_variable_wrap, and thp_variable_unpack functions.
    - Adjusted include statement formatting in torch_python.h for better readability.

--- torch-sys/libtch/torch_python.cpp
+++ torch-sys/libtch/torch_python.cpp
@@ -1,22 +1,16 @@
 #include "torch_python.h"
 
-bool thp_variable_check(PyObject* pyobj) {
-  PROTECT(
-      return THPVariable_Check(pyobj);
-  )
+bool thp_variable_check(PyObject *pyobj) {
+  PROTECT(return THPVariable_Check(pyobj);)
   return false;
 }
 
-PyObject* thp_variable_wrap(tensor t) {
-  PROTECT(
-      return THPVariable_Wrap(*t);
-  )
+PyObject *thp_variable_wrap(tensor t) {
+  PROTECT(return THPVariable_Wrap(*t);)
   return nullptr;
 }
 
-tensor thp_variable_unpack(PyObject* pyobj) {
-  PROTECT(
-      return new torch::Tensor(THPVariable_Unpack(pyobj));
-  )
+tensor thp_variable_unpack(PyObject *pyobj) {
+  PROTECT(return new torch::Tensor(THPVariable_Unpack(pyobj));)
   return nullptr;
 }
--- torch-sys/libtch/torch_python.h
+++ torch-sys/libtch/torch_python.h
@@ -2,15 +2,15 @@
 #define __TORCH_PYTHON_H__
 
 #include "torch_api.h"
-#include<torch/csrc/autograd/python_variable.h>
+#include <torch/csrc/autograd/python_variable.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-bool thp_variable_check(PyObject*);
-PyObject* thp_variable_wrap(tensor);
-tensor thp_variable_unpack(PyObject*);
+bool thp_variable_check(PyObject *);
+PyObject *thp_variable_wrap(tensor);
+tensor thp_variable_unpack(PyObject *);
 
 #ifdef __cplusplus
 };
